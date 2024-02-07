49bdba6567f425b60edee33d4cd9953364b32038
Tan Yi Jun <tanyijun6@gmail.com>
2024-02-08 02:03:11 +0800

    Remove unnecessary items

--- src/index.js
+++ src/index.js
@@ -1,11 +1,1 @@
 import './style.css';
-
-const name = 'James';
-const person = { first: name };
-
-console.log(person);
-
-const sayHelloLinting = (fName) => {
-  console.log(`Hello linting, ${fName}`);
-};
-sayHelloLinting(person.first);
