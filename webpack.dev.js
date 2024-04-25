ddef8a8ed9bc27714b1f98e76ea519bda64c5e5e
Tan Yi Jun <tanyijun6@gmail.com>
2024-04-25 22:03:44 +0800

    Update configuration

--- webpack.dev.js
+++ webpack.dev.js
@@ -10,18 +10,18 @@ module.exports = merge(common, {
   },
   module: {
     rules: [
-      // {
-      //   test: /\.(?:js|mjs|cjs)$/,
-      //   exclude: /node_modules/,
-      //   use: {
-      //     loader: 'babel-loader',
-      //     options: {
-      //       presets: [
-      //         ['@babel/preset-env', { targets: "defaults" }]
-      //       ]
-      //     }
-      //   }
-      // },
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: "defaults" }]
+            ]
+          }
+        }
+      },
     ],
   },
   optimization: {
