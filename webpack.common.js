09af6481cafd436b0d851bf93fb4903c6cb99d9c
Tan Yi Jun <tanyijun6@gmail.com>
2024-07-11 21:00:15 +0800

    Fix html template doesn't load image

--- webpack.common.js
+++ webpack.common.js
@@ -11,6 +11,10 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.html$/i,
+        use: ['html-loader'],
+      },
+      {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
